e74e95c739ccacb97b0f1330a6dc3d34ac494012
Freddy Rodriguez <SaulARodriguez@a2zmacbook.local>
2016-05-16 15:44:23 -0500

    Assign category to plays

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -8,10 +8,12 @@ class PlaysController < ApplicationController
 
   def new
     @play = current_user.plays.build
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
   def create
     @play = current_user.plays.build(play_params)
+    @play.category_id = params[:category_id]
     if @play.save
       redirect_to root_path
     else
@@ -20,9 +22,12 @@ class PlaysController < ApplicationController
   end
 
   def edit
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
   def update
+    @play.category_id = params[:category_id]
+    
     if @play.update(play_params)
       redirect_to @play
     else
@@ -41,7 +46,7 @@ class PlaysController < ApplicationController
     end
     
     def play_params
-      params.require(:play).permit(:title, :description, :director)
+      params.require(:play).permit(:title, :description, :director, :category_id)
     end
   
 end
